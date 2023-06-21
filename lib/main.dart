48b52e1ca8abdb55e3641be3a0e976c287b38ff8
chandansgowda <chandansuresh007@gmail.com>
2023-06-21 17:20:42 +0530

    firebase setup + basic layout

--- lib/main.dart
+++ lib/main.dart
@@ -15,16 +15,57 @@ class MyApp extends StatelessWidget {
 }
 
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  bool isLightOn = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('AutoHome'),
+        title: Text('Ananya'),
         centerTitle: true,
         backgroundColor: Colors.teal,
+      ),
+      body: Container(
+        width: double.infinity,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            SizedBox(height: 20,),
+            SizedBox(
+              height: 300,
+              child: GestureDetector(
+                onTap: (){
+                  setState(() {
+                    isLightOn = !isLightOn;
+                  });
+                },
+                child: Card(child: Column(
+                  children: [
+                    Expanded(child: Image.asset(isLightOn ? "assets/lighton.png" : "assets/lightoff.png")),
+                    Text("Room", style: TextStyle(fontSize: 30),),
+                  ],
+                ),),
+              ),
+            ),
+            SizedBox(height: 20,),
+            SizedBox(
+              height: 300,
+              child: Card(child: Column(
+                children: [
+                  Expanded(child: Image.asset("assets/lightoff.png")),
+                  Text("Hall", style: TextStyle(fontSize: 30),),
+                ],
+              ),),
+            ),
+          ],
+        ),
       ),
     );
   }
